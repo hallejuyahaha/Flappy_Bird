5627c73ac732beb361c09d02b1a233983f257786
hallejuyahaha <357563221@qq.com>
2016-12-28 19:01:41 +0800

    jj

--- js/Flappy_js.js
+++ js/Flappy_js.js
@@ -4,6 +4,11 @@ var img_Bird = document.getElementById("bird");
 var img_Back1 = document.getElementById("back1");
 var img_Back2 = document.getElementById("back2");
 
+//全局变量
+var g = 0.002;//重力加速度
+var fly_Power = -0.6;//飞行力度
+var background_speed = 4;//背景移动速度
+//全局变量结束
 
 //Bird的构造函数
 var Bird = function (img,x,y,speed,ctx){
@@ -17,7 +22,6 @@ var Bird = function (img,x,y,speed,ctx){
 Bird.prototype.draw = function (){
     this.ctx.drawImage(this.img,this.x,this.y,48,42);
 }
-var g = 0.001;
 Bird.prototype.update = function(t){ 
    this.speed = g*t+this.speed;
    this.y += Math.floor(0.5*g*t*t+this.speed*t);;
@@ -47,14 +51,32 @@ backGround.prototype.update = function(){
     }
     this.x = this.x - this.speed;
 }
-
 //背景的构造函数结束
+
+//木桶的构造函数
+var bucket = function(x,y,speed,ctx){
+    
+}
+bucket.prototype.draw = function(){
+    ctx.beginPath();
+    ctx.fillStyle="#F00";/*设置填充颜色*/ 
+    ctx.fillRect(0,0,50,100);
+    ctx.closePath();//可选步骤，关闭绘制的路径
+    ctx.stroke(); //填充
+}
+
+bucket.prototype.update = function(){
+   
+}
+//木桶的构造函数结束
+
+var preTime= Date.now();             //获取当前时间
+var b = new Bird(img_Bird,cvs.width/5,cvs.height/8,0.0003,ctx);//创建小鸟
+var back = new backGround(img_Back1,img_Back2,0,0,background_speed,ctx);//创建背景
+var bucket_one = new bucket(0,0,background_speed,ctx);
 
 
 //主函数
-var preTime= Date.now();             //获取当前时间
-var b = new Bird(img_Bird,cvs.width/2-80,cvs.height/2-35,0.0003,ctx);//创建小鸟
-var back = new backGround(img_Back1,img_Back2,0,0,5,ctx);//创建背景
 function run(){
        var now = Date.now();         //获取最新时间
        dt = now - preTime;            //获取时间间隔
@@ -65,7 +87,7 @@ function run(){
        back.update();
        back.draw();
        //画背景结束
-
+       bucket_one.draw();
        //画小鸟
        b.update(dt);
        b.draw();
@@ -74,7 +96,7 @@ function run(){
    requestAnimationFrame(run);    //再次执行run函数
  }
  cvs.addEventListener("click",function(){
-      b.speed = -0.8;
+      b.speed = fly_Power;
    });
 requestAnimationFrame(run);   //首次执行run函数；
 //主函数结束
